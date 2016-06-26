aa3e29dff7bce379302f148d70ea72dac07637b9
Jan "aso" Szenborn <aso.gdynia@gmail.com>
2016-06-26 17:45:16 +0200

    Working search, small UI changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -26,6 +26,11 @@ MainWindow::MainWindow(QWidget *parent) :
     // QTableWidget last column auto-resizing
     ui->tblResults->horizontalHeader()->setStretchLastSection(true);
 
+    // QTableWidget disabled editing
+    ui->tblResults->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tblResults->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->tblResults->setSelectionBehavior(QAbstractItemView::SelectRows);
+
     // Connect search box signals
     connect(ui->edQuery, SIGNAL(textChanged(QString)), this, SLOT(doSearch()));
 }
@@ -37,5 +42,40 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::doSearch() {
+    // Get text to search
+    QString query = ui->edQuery->text();
 
+    // Clear table for results
+    ui->tblResults->clearContents();
+    ui->tblResults->setRowCount(0);
+
+    // Return if search should't executed
+    if (query.isEmpty())
+        return;
+
+    // Allocate memory for search result
+    TEntriesList result;
+
+    // Selecting proper function
+    if (ui->cbSearchType->currentIndex() == 0) {
+        // Search by callsign
+        result = lookup->searchByCallsign(query);
+    } else {
+        // Search by country
+        result = lookup->searchByCountry(query);
+    }
+
+    // Fill table
+    for (auto it = result.begin(); it != result.end(); it++) {
+        // Get current row count
+        int row = ui->tblResults->rowCount();
+
+        // Insert new row on end
+        ui->tblResults->insertRow(row);
+
+        // Set values on newly added row
+        ui->tblResults->setItem(row, 0, new QTableWidgetItem(it->first.first));
+        ui->tblResults->setItem(row, 1, new QTableWidgetItem(it->first.second));
+        ui->tblResults->setItem(row, 2, new QTableWidgetItem(it->second));
+    }
 }
